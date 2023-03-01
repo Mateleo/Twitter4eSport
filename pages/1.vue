b0a17a543234cb599174877627ee2fc7f7c1c597
Mateleo <mateleo78@gmail.com>
2023-03-01 14:19:50 +0100

    first commit

---
+++ pages/1.vue
@@ -0,0 +1,14 @@
+<script lang=ts setup>
+useServerSeoMeta({
+  title: 'Twitter',
+  ogTitle: 'Twitter',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.zefzefzefe',
+  ogImage: 'https://twitter.4eSport.fr/_ipx/_/img/img.jpg',
+  twitterCard: 'summary_large_image',
+  themeColor:'#0ea5e9'
+})
+
+
+</script>
+<template></template>
