4830c1f304fd6c72ec90620171c1386376c39e4b
Mateleo <mateleo78@gmail.com>
2023-03-01 15:35:43 +0100

    typo

--- pages/1.vue
+++ pages/1.vue
@@ -1,23 +1,22 @@
-<script lang=ts setup>
+<script lang="ts" setup>
 useServerSeoMeta({
-  description: 'This is my amazing site, let me tell you all about it.',
-  ogDescription: 'According to @anonimotum, Upset has reached a verbal agreement with Vitality for next split in the LEC',
-  ogImage: 'https://twitter.4eSport.fr/img/meta.jpg',
-  twitterCard: 'summary_large_image',
-  themeColor:'#0ea5e9'
-})
+  description: "This is my amazing site, let me tell you all about it.",
+  ogDescription:
+    "According to @anonimotum, Upset has reached a verbal agreement with Vitality for next split in the LEC",
+  ogImage: "https://twitter.4eSport.fr/img/meta.jpg",
+  twitterCard: "summary_large_image",
+  themeColor: "#0ea5e9",
+});
 useHead({
-  link:[
-    {type:"application/json+oembed",href:"https://twitter.4eSport.fr/meta.json"}
+  link: [
+    { type: "application/json+oembed", href: "https://twitter.4eSport.fr/meta.json" },
   ],
-  meta:[
-    {property:"og:site_name",content:"Twitter"}
-    {property:"og:type",content:`article`}
-  ]
-})
-
-
+  meta: [
+    { property: "og:site_name", content: "Twitter" },
+    { property: "og:type", content: `article` },
+  ],
+});
 </script>
 <template>
-  <img v-if="false" src="/img/meta.jpg" alt="">
+  <img v-if="false" src="/img/meta.jpg" alt="" />
 </template>
