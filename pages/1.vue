96c63bbe8b6e0a87d2057286cf9deafe86b8f7d2
Mateleo <mateleo78@gmail.com>
2023-03-01 16:44:20 +0100

    test

--- pages/1.vue
+++ pages/1.vue
@@ -2,7 +2,7 @@
 useServerSeoMeta({
   description: "This is my amazing site, let me tell you all about it.",
   ogDescription:
-    "[Sources] ADC Vepo Wunder :flag_fr: will not be playing for Temporary in the GL next split.\n\n It has not been fully decided who will replace him but Ink :flag_fr: is likely to be promoted. \n\n <b>Likes</b>\n 18514",
+    "[Sources] ADC Vepo Wunder :flag_fr: will not be playing for Temporary in the GL next split.\n\n It has not been fully decided who will replace him but Ink :flag_fr: is likely to be promoted. \n\n Likes\n 18514",
   ogImage: "https://twitter.4eSport.fr/img/meta.jpg",
   twitterCard: "summary_large_image",
   themeColor: "#0ea5e9",
