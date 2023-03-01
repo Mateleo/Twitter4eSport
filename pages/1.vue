a9839fc4886ad8ce34815fdafd4dee0c145f7836
Mateleo <mateleo78@gmail.com>
2023-03-01 16:09:28 +0100

    fzefze

--- pages/1.vue
+++ pages/1.vue
@@ -2,7 +2,7 @@
 useServerSeoMeta({
   description: "This is my amazing site, let me tell you all about it.",
   ogDescription:
-    "According to @anonimotum, Upset has reached a verbal agreement with Vitality for next split in the LEC",
+    "According to @anonimotum, Upset has reached a verbal agreement with Vitality for next split in the LEC \n hfuezfhuzeuf",
   ogImage: "https://twitter.4eSport.fr/img/meta.jpg",
   twitterCard: "summary_large_image",
   themeColor: "#0ea5e9",
