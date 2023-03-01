835f0816213f81490b859778d1a99c9c2cd1fcfe
Mateleo <mateleo78@gmail.com>
2023-03-01 17:02:52 +0100

    final

--- pages/216732123123.vue
+++ pages/216732123123.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 useServerSeoMeta({
   description: "This is my amazing site, let me tell you all about it.",
-  ogDescription:"[Sources] Toplaner Wunder 🇫🇷 will not be playing for Fnatic in the LEC next split. It has not been fully decided who will replace him but Oscarinin 🇪🇸 is likely to be promoted.",
+  ogDescription:"[Sources] ADC Vepo will not be playing for Temporary in the GL next split.\n\n It has not been fully decided who will replace him but Ink is likely to be promoted.\n\nLikes\n12734",
   ogImage: "https://twitter.4eSport.fr/img/meta.jpg",
   twitterCard: "summary_large_image",
   themeColor: "#0ea5e9",
