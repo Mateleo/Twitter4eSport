1853dfac0d9c6ab0f420a1acb75276e73d642db1
Mateleo <mateleo78@gmail.com>
2023-03-01 15:13:49 +0100

    meta.json

--- pages/1.vue
+++ pages/1.vue
@@ -1,13 +1,18 @@
 <script lang=ts setup>
 //comment
-useServerSeoMeta({
-  title: 'LEC Wooloo 🇪🇺 (@LEC_Wooloo)',
-  ogTitle: 'Twitter',
-  description: 'This is my amazing site, let me tell you all about it.',
-  ogDescription: 'According to @anonimotum, Upset has reached a verbal agreement with Vitality for next split in the LEC',
-  ogImage: 'https://twitter.4eSport.fr/img/meta.jpg',
-  twitterCard: 'summary_large_image',
-  themeColor:'#0ea5e9'
+// useServerSeoMeta({
+//   title: 'LEC Wooloo 🇪🇺 (@LEC_Wooloo)',
+//   ogTitle: 'Twitter',
+//   description: 'This is my amazing site, let me tell you all about it.',
+//   ogDescription: 'According to @anonimotum, Upset has reached a verbal agreement with Vitality for next split in the LEC',
+//   ogImage: 'https://twitter.4eSport.fr/img/meta.jpg',
+//   twitterCard: 'summary_large_image',
+//   themeColor:'#0ea5e9'
+// })
+useHead({
+  link:[
+    {type:"application/json+oembed",href:"https://twitter.4eSport.fr/meta.json"}
+  ]
 })
 
 
