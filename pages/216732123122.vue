20fd8e3917447a537e406b9bc29207edaf6761a5
Mateleo <mateleo78@gmail.com>
2023-03-09 14:41:10 +0100

    typo

--- pages/216732123122.vue
+++ pages/216732123122.vue
@@ -2,7 +2,7 @@
 useServerSeoMeta({
   description: "This is my amazing site, let me tell you all about it.",
   ogDescription:"[Sources] Jungler Turdyo 🇫🇷 is possibly expected at the rezal this Saturday, March 11.\n\nA verbal agreement has been reached but a deal is yet to be signed.\n\nLikes\n12734",
-  ogImage: "https://twitter.4eSport.fr/img/meta.jpg",
+  ogImage: "https://twitter.4eSport.fr/img/meta2.jpg",
   twitterCard: "summary_large_image",
   themeColor: "#0ea5e9",
   ogType:'website',
