753fcc33342273bdd6723deb72d8b6f7999a2e4c
Mateleo <mateleo78@gmail.com>
2023-03-09 14:36:45 +0100

    new tweet !

---
+++ pages/216732123124.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+useServerSeoMeta({
+  description: "This is my amazing site, let me tell you all about it.",
+  ogDescription:"[Sources] Jungler Turdyo 🇫🇷 is possibly expected at the rezal this Saturday, March 11.\n\nA verbal agreement has been reached but a deal is yet to be signed.\n\nLikes\n12734",
+  ogImage: "https://twitter.4eSport.fr/img/meta.jpg",
+  twitterCard: "summary_large_image",
+  themeColor: "#0ea5e9",
+  ogType:'website',
+});
+useHead({
+  link: [
+    { type: "application/json+oembed", href: "https://twitter.4eSport.fr/meta.json" },
+  ],
+});
+</script>
+<template>
+  <img v-if="false" src="/img/meta2.jpg" alt="" />
+</template>
