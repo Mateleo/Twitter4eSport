bb1d12c3f49350c585d6384102188299f9b28ac8
Mateleo <mateleo78@gmail.com>
2023-03-31 22:36:23 +0200

    update

--- pages/216732123121.vue
+++ pages/216732123121.vue
@@ -2,7 +2,7 @@
 useServerSeoMeta({
   description: "This is my amazing site, let me tell you all about it.",
   ogDescription:
-    "[Sources] TOP Manguier 🇫🇷 will be back for April 1 in SoloQ.\n\nAfter being Gold in S9, Diamond in S10 and Master in S11 and S12, Manguier promises to be Grandmaster for this season according to his Discord profile.\n\nDespite a brilliant rise and a constant will to improve, the player was never able to express himself as a Baron level during the Grosse Ligue.\n\nTOP Manguier 🇫🇷 never confirmed or denied his willingness to participate in the GL next year.\n\n\n\nLikes\n12734",
+    "[Sources] TOP Manguier 🇫🇷 will be back for April 1 in SoloQ.\n\nAfter being Gold in S9, Diamond in S10 and Master in S11 and S12, he promises to be GM for this season according to his Discord profile.\nDespite a brilliant rise and a constant will to improve, the player was never able to express himself as a Baron level during Grosse Ligue.\nTOP Manguier 🇫🇷 never confirmed or denied his willingness to participate in the GL next year.",
   ogImage: "https://twitter.4eSport.fr/img/meta3.jpg",
   twitterCard: "summary_large_image",
   themeColor: "#0ea5e9",
