fb3956aabe91652429b4ccd2118b7367ff4c7250
Mateleo <mateleo78@gmail.com>
2023-05-07 22:46:01 +0200

    ✨ feat(216732123125.vue): add server-side SEO meta tags and oEmbed link tag

--- pages/216732123125.vue
+++ pages/216732123125.vue
@@ -2,7 +2,7 @@
 useServerSeoMeta({
   description: "This is my amazing site, let me tell you all about it.",
   ogDescription: "[Sources] Support Colin, proposed in an interview for ADC Mateleo, a list of nerfs to apply urgently.\nDivine Sunderer, Navori, Support roaming, but also the removal of Zeri and Yuumi in the game.\n\nA complete list of changes will be available in his book.",
-  ogImage: "https://twitter.4eSport.fr/img/meta.jpg",
+  ogImage: "https://twitter.4eSport.fr/img/meta4.jpg",
   twitterCard: "summary_large_image",
   themeColor: "#0ea5e9",
   ogType: 'website',
