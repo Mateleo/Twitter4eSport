31dadb41ad91480c8c51bc3e2ddd77a5518a980f
Mateleo <mateleo78@gmail.com>
2023-08-27 22:33:51 +0200

    🚀 chore(package.json): update Nuxt dependency to version 3.7.0 📝 docs(meta.json): update author_name field to "P4ND4 P0W3R (@4eSP4ND4P0W3R)" for better accuracy 🎨 style(216732123121.vue): format code for better readability 🖼️ feat(panda.jpg): add new panda.jpg image to the public/img directory 📝 docs(metapanda.json): create metapanda.json file with metadata for panda image

---
+++ pages/216732123121.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+useServerSeoMeta({
+  description: "This is my amazing site, let me tell you all about it.",
+  ogDescription:
+    "'Je veux marquer le coup'\n\nEn tant que nouveau président de 4eSport, je suis heureux de vous annoncer un CP de 350€ supplémentaire pour un total de 500€.\n\nOn se retrouve le 5 septembre à 21h !",
+  ogImage: "https://twitter.4eSport.fr/img/panda.jpg",
+  twitterCard: "summary_large_image",
+  themeColor: "#0ea5e9",
+  ogType: "website",
+});
+useHead({
+  link: [
+    { type: "application/json+oembed", href: "https://twitter.4eSport.fr/metapanda.json" },
+  ],
+});
+</script>
+<template>
+  <img v-if="false" src="/img/panda.jpg" alt="" />
+</template>
