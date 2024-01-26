8527b2b6b337437209128e060ac5a3c1898a7f33
Mateleo <mateleo78@gmail.com>
2024-01-26 20:28:19 +0100

    test

---
+++ pages/216732123126.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+useServerSeoMeta({
+  description: "This is my amazing site, let me tell you all about it.",
+  ogDescription:
+    "[Source] Leaked screen proves Robert is (still) better.\n\nOnce again, the rivalry between Robébou and Dioubot has been expressed in a new game: Clash of Clans.\nThis time, Robert seems to have come out on top, according to a screen leaked on several Discord servers.",
+  ogImage: "https://twitter.4eSport.fr/img/coc.png",
+  twitterCard: "summary_large_image",
+  themeColor: "#0ea5e9",
+  ogType: "website",
+});
+useHead({
+  link: [{ type: "application/json+oembed", href: "https://twitter.4eSport.fr/meta4esport.json" }],
+});
+</script>
+<template>
+  <img v-if="false" src="/img/coc.png" alt="" />
+</template>
