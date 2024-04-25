4730715427c45b527d12047aaa5e39b29eb1687f
Mateleo <mateleo78@gmail.com>
2024-04-25 15:44:48 +0200

    typo

--- pages/216732123131.vue
+++ pages/216732123131.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 useServerSeoMeta({
   description: "This is my amazing site, let me tell you all about it.",
-  ogDescription: "[Sources] Support Lew was reportedly recruited by Vitaly in the middle of the season.\n\nHis teammate Turdyo describes the icy atmosphere in the team during the Télécom Paris tournament. “We knew he was leaving”.\nHis team captain Dioutbot blames the player more directly: “He rundown us during the final, he didn't give a damn anymore”.\n",
+  ogDescription: "[Sources] Support Lew was reportedly recruited by Vitaly in the middle of the season.\n\nHis teammate Turdyo describes the icy atmosphere in the team during the Télécom Paris tournament. “We knew he was leaving”.\nCaptain team Dioutbot blames the player more directly: “He rundown us during the final, he didn't give a damn anymore”.\n",
   ogImage: "https://twitter.4eSport.fr/img/boukada.png",
   twitterCard: "summary_large_image",
   themeColor: "#0ea5e9",
