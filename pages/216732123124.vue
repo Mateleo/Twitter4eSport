918a73b179c9c9ff6978a7c653821d0151ae650a
Mateleo <mateleo78@gmail.com>
2024-04-25 15:36:55 +0200

    lew

---
+++ pages/216732123124.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+useServerSeoMeta({
+  description: "This is my amazing site, let me tell you all about it.",
+  ogDescription: "[Sources] Support Lew was reportedly recruited by Vitaly in the middle of the season.\nHis teammate Turdyo describes the icy atmosphere in the team during the Télécom Paris tournament. “We knew he was leaving, so we tried to get past it”. His team captain Dioutbot blames the player more directly: “He rundown us during the final, he didn't give a damn anymore”.\nThe Support did not wish to answer our questions.",
+  ogImage: "https://twitter.4eSport.fr/img/meta4.jpg",
+  twitterCard: "summary_large_image",
+  themeColor: "#0ea5e9",
+  ogType: 'website',
+});
+useHead({
+  link: [
+    { type: "application/json+oembed", href: "https://twitter.4eSport.fr/meta.json" },
+  ],
+});
+</script>
+<template>
+  <img v-if="false" src="/img/boukada.png" alt="" />
+</template>
