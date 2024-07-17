547613c554cbaff7743e4d4afc33c48b99b89a66
Mateo Siam Dominguez <mateo-siam.dominguez@airbus.com>
2024-07-17 17:36:53 +0200

    flight

---
+++ pages/216732123129.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+useServerSeoMeta({
+  description: "This is my amazing site, let me tell you all about it.",
+  ogImage: "https://twitter.4eSport.fr/img/flight.jpg",
+  twitterCard: "summary_large_image",
+  themeColor: "#0ea5e9",
+  ogType: "website",
+});
+useHead({
+  link: [{ type: "application/json+oembed", href: "https://twitter.4eSport.fr/meta4esport.json" }],
+});
+</script>
+<template>
+  <img v-if="false" src="/img/flight.jpg" alt="" />
+</template>
