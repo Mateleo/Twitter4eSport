e134d9c6af64c6de7c20651432a2a8b531fd3c1a
Mateo Siam Dominguez <mateo-siam.dominguez@airbus.com>
2024-07-17 17:41:32 +0200

    typo

--- pages/216732123130.vue
+++ pages/216732123130.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 useServerSeoMeta({
   description: "This is my amazing site, let me tell you all about it.",
+  ogDescription:
+  "\n\n🛫 Il faut faire un choix.",
   ogImage: "https://twitter.4eSport.fr/img/flight.jpg",
   twitterCard: "summary_large_image",
   themeColor: "#0ea5e9",
