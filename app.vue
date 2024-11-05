50193ef1e9cd4686ff6385f5e74cea470ca4a7f0
Mateleo <mateleo78@gmail.com>
2024-11-05 19:00:57 +0100

    update

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
+  <div class="h-full">
     <NuxtPage></NuxtPage>
   </div>
 </template>
+<style>
+body, html, #__nuxt{
+  height: 100%;
+  width: 100%;
+}
+
+</style>
---
+++ pages/216732123134.vue
@@ -0,0 +1,61 @@
+<script lang="ts" setup>
+useSeoMeta({
+  description: "This is my amazing site, let me tell you all about it.",
+  ogDescription:
+    "\n\nDEBRIEF LIVE MOUAMEME - AFFAIRE ELITE/DOLEX\n\n(Rien n'est mon avis juste la retranscription de Mouameme)\n\n\n- Dioubot ne supporte plus le faible niveau de Turdyo\n- Celui-ci le voit comme une trahison.",
+  twitterCard: "summary_large_image",
+  themeColor: "#0ea5e9",
+  ogType: "website",
+});
+useHead({
+  link: [{ type: "application/json+oembed", href: "https://twitter.4eSport.fr/metaJen.json" }],
+});
+</script>
+<template>
+  <div class="size-full bg-black text-white">
+    <div class="md:max-w-[50%] w-[90%] m-auto border border-b-0 h-full border-white/20 px-4">
+      <div class="flex flex-col mt-12">
+        <div class="flex gap-2 items-center">
+          <img
+            src="https://pbs.twimg.com/profile_images/1848069317008441344/8Z-74Trh_400x400.jpg"
+            class="rounded-full size-[32px]"
+            alt=""
+          />
+          <div class="flex flex-col">
+            <div class="flex items-center">
+              <p class="font-bold text-sm">Jennysis ✨</p>
+              <Icon name="solar:verified-check-bold" class="text-[#1d9bf0]"></Icon>
+            </div>
+            <p class="text-sm opacity-30 font-light -mt-1">@Jennysis__</p>
+          </div>
+        </div>
+        <div class="flex flex-col">
+          <p class="mt-2 mb-2 font-medium">DEBRIEF LIVE MOUAMEME - AFFAIRE ELITE/DOLEX</p>
+          <p class="mb-6">(Rien n'est mon avis juste la retranscription de Mouameme)</p>
+          <p class="leading-5">- Dioubot ne supporte plus le faible niveau de Turdyo</p>
+          <p class="leading-5">
+            - Celui-ci le voit comme une trahison et je cite "de tout facon dioubot a rundown des scrims il se met mute
+            casque sur discord ce joueur n'est pas serieux"
+          </p>
+          <p class="leading-5">- Lew n'aurait pas quitté l'équipe mais aurait été bench (départ forcé)</p>
+          <p class="leading-5">- Bvig souffre de voir son équipe se déchirer</p>
+          <p class="leading-5">- Manguier n'a pas été pris dans Elite et le vis comme une injustice</p>
+          <p class="leading-5">- Jiah non plus (Colin véto ?)</p>
+          <p class="leading-5">- Lew aurait monté la tête de Doser contre le reste des joueurs (présent au TO)</p>
+          <p class="leading-5">- Doser ne s'est pas encore exprimé sur le sujet</p>
+          <p class="leading-5">- Robert en embuscade pour récuper un slot ?</p>
+          <p class="mt-6">- (coupure du live, je rajouterai en dessous si suite)</p>
+          <p class="mt-6 leading-5">
+            - (Petit message perso : ça se ressent que Mouameme se livre totalement et est vraiment touché par tout ça,
+            j'espère vraiment que 4eSport pourra gagner la GL, elle la mérite ❤️)
+          </p>
+        </div>
+        <p class="text-white/50 font-thin text-sm mt-4">
+          9:22 PM · Nov 4, 2024 · <span class="font-medium text-white">2.1M</span> Views
+        </p>
+        <div class="border-y border-white/20 h-12 mt-4"</div>
+      </div>
+      <div></div>
+    </div>
+  </div>
+</template>
