68f46c43433228c9429b9c1d91fbe5127824a419
Mateleo <mateleo78@gmail.com>
2025-03-07 21:57:46 +0100

    turdybucket

---
+++ pages/216732123134.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+useServerSeoMeta({
+  description: "This is my amazing site, let me tell you all about it.",
+  ogDescription: "Turdyo's piss bucket is 90% full! Please let him know as soon as possible!\n\nLikes\n1294",
+  ogImage: "https://twitter.4eSport.fr/img/bucket.jpg",
+  twitterCard: "summary_large_image",
+  themeColor: "#0ea5e9",
+  ogType: "website",
+});
+useHead({
+  link: [{ type: "application/json+oembed", href: "https://twitter.4eSport.fr/meta4esport.json" }],
+});
+</script>
+<template>
+  <img v-if="false" src="/img/boukada.png" alt="" />
+</template>
