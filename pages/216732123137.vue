65060fe6db37a2878cbd197ec252a021ed1308a8
Mateleo <mateleo78@gmail.com>
2025-03-15 20:27:57 +0100

    test

--- pages/216732123137.vue
+++ pages/216732123137.vue
@@ -2,7 +2,7 @@
 useServerSeoMeta({
   description: "This is my amazing site, let me tell you all about it.",
   ogDescription:
-    "Trump imposes 25% tariff on Turdyo'piss bucket imports, effective immediately.\nPresident Donald Trump on Friday announced an immediate 25% tariff on all Turdyo'piss bucket imports, citing unfair trade practices. In a post on Truth Social, Trump said the move was necessary to protect American manufacturers from “cheap, low-quality Turdyo’piss flooding our market.”",
+    "Trump imposes 25% tariff on Turdyo'piss bucket imports, effective immediately. President Donald Trump on Friday announced an immediate 25% tariff on all Turdyo'piss bucket imports, citing unfair trade practices. In a post on Truth Social, Trump said the move was necessary to protect American manufacturers from “cheap, low-quality Turdyo’piss flooding our market.”",
   ogImage: "https://twitter.4eSport.fr/img/trump-executive-order.webp",
   twitterCard: "summary_large_image",
   themeColor: "#0ea5e9",
