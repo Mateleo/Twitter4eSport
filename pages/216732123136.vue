ce29c448ceca4c143e17e9ce96e66a0df62248d7
Mateleo <mateleo78@gmail.com>
2025-07-23 16:56:22 +0200

    test

---
+++ pages/216732123136.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+useServerSeoMeta({
+  description: "share a piece of lore about yourself.",
+  ogDescription: "\nShare a piece of lore about yourself.\n",
+  ogImage: "https://twitter.4eSport.fr/img/share.jpg",
+  twitterCard: "summary_large_image",
+  themeColor: "#0ea5e9",
+  ogType: "website",
+});
+useHead({
+  link: [{ type: "application/json+oembed", href: "https://twitter.4eSport.fr/meta4esport.json" }],
+});
+</script>
+<template>
+  <img v-if="false" src="/img/boukada.png" alt="" />
+</template>
