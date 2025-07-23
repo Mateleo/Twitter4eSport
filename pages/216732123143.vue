07c92fbad4d49e1f2d8420c7ab619965aea36f5f
Mateleo <mateleo78@gmail.com>
2025-07-23 17:03:00 +0200

    test

--- pages/216732123143.vue
+++ pages/216732123143.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 useServerSeoMeta({
   description: "This is my amazing site, let me tell you all about it.",
-  ogDescription: "\nshare a piece of lore about yourself\n",
+  ogDescription: "\nshare a piece of 4eSport lore about yourself\n",
   ogImage: "https://twitter.4eSport.fr/img/share.jpg",
   twitterCard: "summary_large_image",
   themeColor: "#0ea5e9",
