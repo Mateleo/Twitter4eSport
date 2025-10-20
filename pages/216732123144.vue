170cd34be71a4bd0d6a05f400d1f7574256538e9
Mateleo <mateleo78@gmail.com>
2025-10-20 20:09:01 +0200

    test

--- pages/216732123144.vue
+++ pages/216732123144.vue
@@ -3,7 +3,6 @@ useServerSeoMeta({
   description: "This is my amazing site, let me tell you all about it.",
   ogDescription:
     "\nManguier, dont la fraude à la Grosse Ligue est assurée, défend Fugu en minimisant les violences physiques contre Helydia, arguant qu'elle aurait \"tabassé une vitre et lui-même\" avant de recevoir une gifle en réponse. Selon lui, la relation toxique est mutuelle, et Helydia, instable émotionnellement, porterait seule la responsabilité de l'escalade.",
-  ogImage: "https://twitter.4eSport.fr/img/share.jpg",
   twitterCard: "summary_large_image",
   themeColor: "#0ea5e9",
   ogType: "website",
